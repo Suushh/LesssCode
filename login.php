b2dcf761ecbefe265c495d185078d375e17f34d2
Shruti <152834711+Suushh@users.noreply.github.com>
2024-07-12 19:25:57 +0530

    Update login.php

--- login.php
+++ login.php
@@ -14,14 +14,67 @@ session_start();
     crossorigin="anonymous">  
     <style>
         .content {
-            padding-top: 20%;
+            font-family: 'Arial', sans-serif;
+            background: rgb(255, 227, 232);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            padding: 0;
         }
+        .container {
+            background-color: #fff;
+            border-radius: 10px;
+            display: flex;
+            flex-direction: column;
+            width: 400px;
+            justify-content: space-around;
+            min-height: 480px;
+            margin: 0;
+        }
+        h1 {
+            font-weight: bold;
+            
+        }
+        p {
+            font-size: 14px;
+        
+            margin: -10px 30px;
+        }
+        .btn-custom {
+            border: 1px solid white;
+            background-color: rgb(202, 6, 104);
+            color: #FFFFFF;
+            font-size: 12px;
+            font-weight: bold;
+            padding: 12px 45px;
+            letter-spacing: 1px;
+            text-transform: uppercase;
+            transition: transform 80ms ease-in;
+            cursor: pointer;
+        }
+        .btn-custom:active {
+            transform: scale(0.95);
+        }
+        .btn-custom:focus {
+            outline: none;
+        }
+        a {
+    color: rgb(202, 6, 104) ;
+    text-decoration: none;
+    font-weight: bold;
+}
+
+a:hover {
+    text-decoration: underline;
+}
     </style> 
 </head>
 <body>
     <div class="content">
         <div class="row justify-content-center">
-            <div class="col-md-6 col-md-offset-3" align="center" >
+            <div align="center">
                 <?php
                     if (isset($_POST["Login"])){
                         $usern=$_POST["username"];
@@ -42,19 +95,21 @@ session_start();
                             }else{
                                 echo "<div class='alert alert-danger'>Wrong Password!</div>";
                             }
-                        }
-                        else{
+                        } else {
                             echo "<div class='alert alert-danger'>U are not signed up!</div>";
                         }
                     }
                 ?>
-                 <form action="login.php" method="POST" action="home.php">
-                    <input type="text" name="username" class="form-control" placeholder="Enter username"><br>
-                    <input type="password" name="password" class="form-control" placeholder="Enter password"><br>
-                    <input type="submit" name="Login" value="Login" class="btn btn-primary">
-                 </form>               
-                 <br>Haven't signed up already?Sign up here!!<br>
-                 <a href="sign_up.php">Sign up</a>
+                <div class="container">
+                    <h1>Login</h1>
+                    <form action="login.php" method="POST">
+                        <input type="text" name="username" class="form-control" placeholder="Enter username"><br>
+                        <input type="password" name="password" class="form-control" placeholder="Enter password"><br>
+                        <input type="submit" name="Login" value="Login" class="btn-custom"><br>
+                    </form>               
+                    <p>Haven't signed up already? <a href="sign_up.php">Sign up</a></p>
+                    
+                </div>
             </div> 
         </div>
     </div>
